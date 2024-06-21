7f0a70819ec48728a1b283cb9d67837e77f72be7
Titas <titaslr38@gmail.com>
2024-06-21 16:51:35 +0300

    Add reset button

--- scripts.js
+++ scripts.js
@@ -1,7 +1,19 @@
 const container = document.querySelector(".container");
+const button = document.querySelector(".reset");
+
+function changeColor(item){
+    item.style.backgroundColor = "black";
+}
+
+function resetGrid(){
+    console.log("worked");
+}
 
 for (let i = 0; i < 256; i++){
     const box = document.createElement("div");
     box.classList.add("box");
+    box.addEventListener("mouseover", () => {changeColor(box)});
     container.appendChild(box);
 }
+
+button.addEventListener("click", resetGrid);
