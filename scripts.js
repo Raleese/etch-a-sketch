728c937aa7e4e0309700009eb5def9304fa41213
Titas <titaslr38@gmail.com>
2024-06-21 17:57:30 +0300

    Add a reset grid button that lets users set the grid size

--- scripts.js
+++ scripts.js
@@ -6,7 +6,17 @@ function changeColor(item){
 }
 
 function resetGrid(){
-    console.log("worked");
+    let size = prompt("Enter a new grid");
+    document.querySelectorAll(".box").forEach(e => e.remove());
+
+    for (let i = 0; i < size*size; i++){
+        const box = document.createElement("div");
+        box.classList.add("box");
+        box.style.height = (800/size) - 2 + "px";
+        box.style.width = (800/size) - 2 + "px";
+        box.addEventListener("mouseover", () => {changeColor(box)});
+        container.appendChild(box);
+    }
 }
 
 for (let i = 0; i < 256; i++){
