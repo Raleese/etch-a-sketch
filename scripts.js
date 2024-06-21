a35ca2f780c4f6c84e3e8b94bcc24b3c2b024cc6
Titas <titaslr38@gmail.com>
2024-06-21 16:39:34 +0300

    Add a 16x16 grid

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++){
+for (let i = 0; i < 256; i++){
     const box = document.createElement("div");
     box.classList.add("box");
+    container.appendChild(box);
 }
